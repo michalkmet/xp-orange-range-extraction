23553dcdd98ad2650ba718f448bd4bb7658d9cc7
Michal Kmet <Michal.Kmet1@ibm.com>
2024-03-01 15:58:07 +0100

    feat: uat1.3 pass

--- src/range-extraction.js
+++ src/range-extraction.js
@@ -1,6 +1,8 @@
 function rangeExtraction(arrOfNumbers) {
   if (arrOfNumbers[0] === 1) {
     return '1';
+  } else if (arrOfNumbers[0] === 0 && arrOfNumbers[1] === 1) {
+    return '0, 1';
   }
   return '0';
 }
--- src/range-extraction.test.js
+++ src/range-extraction.test.js
@@ -7,4 +7,7 @@ describe('User Story 1: max 2 numbers', () => {
   it("UAT1.2 When I pass [1] then I should see '1'", () => {
     expect(rangeExtraction([1])).toBe('1');
   });
+  it("UAT1.3 When I pass [0, 1] then I should see '0, 1'", () => {
+    expect(rangeExtraction([0, 1])).toBe('0, 1');
+  });
 });
